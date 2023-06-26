4052a96e79211c58683e1dfd122112cc3807ff7e
Sami Alavi <32700289+SamiAlavi@users.noreply.github.com>
2023-06-26 14:17:58 +0200

    write swagger config json

--- src/swagger/swaggerRoute.ts
+++ src/swagger/swaggerRoute.ts
@@ -3,15 +3,24 @@ import swaggerConfig from './swaggerConfig';
 import swaggerJSDoc from 'swagger-jsdoc';
 import { Request, Response, Router } from 'express';
 import AppSettings from '../../AppSettings';
+import fileService from '../services/fileService';
 
 const router = Router();
 const swaggerSpec = swaggerJSDoc(swaggerConfig);
 
+function writeSwaggerConfig(filePath: string) {
+    const swaggerJson = JSON.stringify(swaggerSpec, null, 4);
+    fileService.writeFileSync(filePath, swaggerJson);
+}
+
 router.get(AppSettings.RouteSwaggerJSON, (req: Request, res: Response) => {
     res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(swaggerSpec, null, 4));
+    const swaggerJson = JSON.stringify(swaggerSpec, null, 4);
+    res.send(swaggerJson);
 });
 
 router.use(AppSettings.RouteSwagger, swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// writeSwaggerConfig('swagger.json');
 
 export default router;
